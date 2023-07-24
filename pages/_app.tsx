b8f3f9b7082dacaf48f15500f553a1fb3955c936
Батсайхан Энхбилгүүн <HP233073@M301-CC0130.local>
2023-07-24 15:26:17 +0800

    adding

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,41 +1,13 @@
-import  { configureAbly, useChannel } from "@ably-labs/react-hooks";
-import { useState } from "react";
-import * as React from "react"
-
-
-configureAbly({key : "Gk_6OA.L15T4Q:Iboyq9-_wQBDusD7444A8nZ0piAh20AnamsrCiblLZ4", clientId: Date.now() + ""});
-
-export default function Home(){
-
-  interface Message {
-    time: number;
-    text: string;
-
-   }
-
-  const [text, setText] = useState<String>("");
-  const [messages, setMessages] = React.useState<Array<Message>>([]);
-
-  const [channel] = useChannel("public-chat", (message) => {
-    setMessages((prev : any ) => [...prev, message]);
-  })
-
-  async function sendMessage(){
-    channel.publish("message", {text, date:Date.now()});
-    setText('')
-  }
-
-  return(
-    <main>
-      {messages.map((message : any) => (
-        <div>
-          <div>
-            {message.data.text}
-          </div>
-        </div>
-      ))}
-      <textarea onChange={(e) => setText(e.target.value)} />
-      <button type="button" onClick={sendMessage}>Send</button>
-    </main>
-  )
+// page/_app.tsx
+import { ClerkProvider } from "@clerk/nextjs";
+import type { AppProps } from "next/app";
+ 
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 }
+ 
+export default MyApp;
